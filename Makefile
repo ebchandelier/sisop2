3db09f2240d084fb88d3915795f9de7ebe7a0e8d
Gabriel Allegretti <gallegretti@inf.ufrgs.br>
2018-04-21 16:25:26 -0300

    Add arguments to server

--- Makefile
+++ Makefile
@@ -1,8 +1,8 @@
 client:
-	g++ -o dropboxClient.out ./src/client/dropboxUtil.cpp ./src/client/dropboxClient.cpp ./src/shared/datagram.h
+	g++ -o dropboxClient.out ./src/client/dropboxClient.cpp ./src/client/dropboxUtil.cpp ./src/shared/datagram.h
 
 server:
-	g++ -o dropboxServer.out ./src/server/dropboxServer.cpp ./src/shared/datagram.h
+	g++ -o dropboxServer.out ./src/server/dropboxServer.cpp ./src/shared/datagram.h ./src/server/ServerConnectorUDP.cpp
 
 clean:
-	rm dropboxClient
+	rm ./dropboxClient.out rm ./dropboxServer.out
--- src/server/dropboxServer.cpp
+++ src/server/dropboxServer.cpp
@@ -1,6 +1,39 @@
 #include "ServerConnectorUDP.h"
 
-int main()
+int main(int argc, char **argv)
 {
+	printf("Initializing server...\n");
+	int port;
 
+	if(argc < 3) {
+		  printf("How to use:\n");
+		  printf("dropboxServer -p <port>\n");
+		  exit(1);
+	 }
+    for(int i=1; i<argc; i++) {
+		if(argv[i][0]=='-') {
+			switch(argv[i][1]) {
+				case 'p':
+					i++;
+					port = atoi(argv[i]);
+					if(port < 1024) {
+						printf("Invalid port value\n");
+						exit(1);
+					}
+					break;
+						  
+				default:
+					printf("Invalid parameter %d: %s\n", i, argv[i]);
+					exit(1);
+			}
+		} else {
+			printf("Invalid parameter %d: %s\n", i, argv[i]); 
+			exit(1);
+		}
+	}
+	printf("Initializing UDP stack");
+    ServerConnectorUDP connector = ServerConnectorUDP();
+	connector.init(port);
+
+	connector.receive_next_package();
 }
