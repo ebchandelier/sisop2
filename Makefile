f7a54cb4247237741cd1dec12c470669b09627c3
Eduardo BC <ebchandelier@gmail.com>
2018-04-23 18:37:35 -0300

    Added Class to read/write files to/from datagrams

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ client:
 	g++ -std=c++11 -o dropboxClient.out ./src/client/dropboxClient.cpp ./src/client/ClientConnectionManager.cpp ./src/client/ClientConnectorUDP.cpp ./src/client/ClientUI.cpp ./src/shared/datagram.h ./src/shared/DatagramStringifier.cpp
 
 server:
-	g++ -std=c++11 -o dropboxServer.out -pthread ./src/server/dropboxServer.cpp ./src/shared/datagram.h ./src/server/ServerConnectorUDP.cpp ./src/shared/DatagramStringifier.cpp ./src/server/ClientHandler.cpp
+	g++ -std=c++11 -o dropboxServer.out -pthread ./src/server/dropboxServer.cpp ./src/shared/* ./src/server/ServerConnectorUDP.cpp ./src/server/ClientHandler.cpp
 
 clean:
 	rm ./dropboxClient.out rm ./dropboxServer.out
---
+++ src/shared/PersistenceFileManager.cpp
@@ -0,0 +1,122 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cstring>
+#include <vector>
+
+#include "datagram.h"
+
+class PersistenceFileManager {
+
+public:
+    
+    std::vector<datagram> read(std::string fileName) {
+
+        char memblock[sizeOfFile(fileName)];
+        std::vector<datagram> datagramVector;
+        std::streampos size = readToBuffer (fileName, memblock);
+
+        if(size == 0) {
+         
+            std::cout << "Error: could not read file " << fileName << "\n";
+        
+        } else {
+
+            int indexToStartReading = 0;
+
+            while(size > DATA_BUFFER_SIZE) {
+
+                datagram dataGram = createSingleDatagram(DATA_BUFFER_SIZE, memblock, indexToStartReading++);
+
+                datagramVector.push_back(dataGram);
+
+                size -= DATA_BUFFER_SIZE;
+            }
+
+            if(size > 0) {
+
+                datagram dataGram = createSingleDatagram(size, memblock, indexToStartReading++);
+
+                datagramVector.push_back(dataGram);
+            }
+
+        }
+
+        return datagramVector;
+    }
+    
+    void write(std::string fileName, std::vector<datagram> data) {
+        
+        int size = 0;
+
+        for (auto datagram : data) {
+            
+            size += datagram.data.data_length;
+        }
+
+        char buffer[size];
+
+        for( int i=0; i<data.size(); i++) {
+            
+            int dataSize = data.at(i).data.data_length;
+            memcpy(buffer + i*DATA_BUFFER_SIZE, data.at(i).data.buffer, dataSize);
+        }
+
+        std::ofstream file (fileName, std::ios::out| std::ios::ate);
+        file.write(buffer, size);
+
+        file.close();
+    }
+
+private:
+
+    std::streampos readToBuffer(std::string fileName, char* buffer) {
+        
+        std::streampos size;
+        std::ifstream file (fileName, std::ios::in| std::ios::ate);
+        
+        if (file.is_open()) {
+
+            size = file.tellg();
+            file.seekg (0, std::ios::beg);
+            file.read (buffer, size);
+            file.close();
+        }
+
+        return size;
+    }
+
+    datagram createSingleDatagram(std::streampos size, char* memblock, int indexToStartReading) {
+        
+        // std::cout << "Creating data_gram de " << size << " bytes\n";
+
+        data_datagram data;
+        data.data_length = size;
+
+        data.package_id = indexToStartReading;
+        std::memcpy(data.buffer, memblock + indexToStartReading*DATA_BUFFER_SIZE, size);
+
+        datagram dataGram;
+        dataGram.type = datagram_type::data;
+        dataGram.data = data;
+
+        return dataGram;
+    }
+
+    int sizeOfFile(std::string fileName) {
+        
+        std::streampos size = 0;
+        std::ifstream file (fileName, std::ios::in| std::ios::ate);
+        
+        if (file.is_open()) {
+
+            size = file.tellg();
+            file.close();
+        }
+
+        return size;
+    }
+};
+
+// class PersistClient extends PersistenceFileManager? To manage userName folder etc
+// class PersistServer extends PersistenceFileManager
